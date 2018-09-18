d39f61ca084f8451bd45c8865e36aa11d593dfa3
Sufiyan Ghori <sufiyanghori@outlook.com>
2018-09-18 18:23:26 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# sensu-plugins-vault
-Sensu Plugins for hashicorp vault
+## Sensu-Plugins-Vault
+
+## Functionality
+
+Checks for the validity of tokens in vault server through API.
+
+## Files
+  * bin/check-vault-tokens.py
+
+## Installation
+
+This plugin make use of vault accessors to get information about indiviual tokens.
+In order for this to work a token with following policies applied is required in vault, 
+
+```path "auth/token/accessors/*"
+{
+  capabilities = ["sudo", "list"]
+}
+
+path "auth/token/lookup-accessor"
+{
+  capabilities = ["update"]
+}
+```
+
+Once the token is generated, create a config file in `/etc/sensu/conf.d` with the following content, replacing `token` and `api_address` with your own configuration:
+
+```{
+  "vault_config": {
+    "token": "abcdea4-2543f-b12543-01221-f721fab128cdd",
+    "api_address": "https://<vault-api>:443"
+  }
+} 
+```
+
+
+## Usage
+
+Create a check file in `/etc/sensu/conf.d`,
+
+```{
+  "checks": {
+    "vault_token_expiry": {
+      "command": "/opt/sensu/embedded/bin/check-vault-tokens.py -c 15",
+      "interval": 5,
+      "subscribers": [
+        "CentOS"
+      ],
+      "standalone": true
+    }
+  }
+}
+```
+
+`-c` flag is used to set the threshold(in days). It triggers when any token is expiring in that number of days.
---
+++ bin/check-vault-tokens.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python 
+#   check-vault-tokens
+#
+# DESCRIPTION:
+# Check validity time of tokens using Vault API and report when tokens are expiring
+#
+# OUTPUT:
+#   text
+#
+# PLATFORMS:
+#   Linux, Windows, BSD, Solaris, etc
+#
+# DEPENDENCIES:
+#   gem: sensu_plugin
+#
+# USAGE:
+#     # RED
+#
+# NOTES:
+#
+# LICENSE:
+#   Sufiyan Ghori  sufiyan@protonmail.com
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
+#
+
+from __future__ import print_function
+from sensu_plugin import SensuPluginCheck
+from sensu_plugin import utils
+import requests
+import sys
+import json
+import dateutil.parser
+import datetime
+
+class VaultTokenExpire(SensuPluginCheck):
+
+  def setup(self):
+
+    self.parser.add_argument(
+      "-c",
+      "--critical",
+      required=True,
+      type=int,
+      help='trigger critical alert when any token is expiring in this number of days'
+    )
+
+  def run(self):
+
+    self.check_name('vault_token_expire')
+
+    VAULT_SERVER = utils.get_settings()['vault_config']['api_address']
+    
+    """ 
+    token must have sudo,list access to auth/token/accessors,
+    and update access to auth/token/lookup-accessor
+    """
+    VAULT_AUTH_TOKEN = utils.get_settings()['vault_config']['token']
+
+
+    """
+    number of days before critical is fired
+    """
+    THRESHOLD_CRITICAL = self.options.critical
+    
+    API_HEADER = {
+        'x-vault-token': VAULT_AUTH_TOKEN, 
+        'content-type': "application/json"
+      }
+
+
+    API_ENDPOINT = {
+        "list_all_accessors": VAULT_SERVER + "/v1/auth/token/accessors", 
+        "accessor_data": VAULT_SERVER + "/v1/auth/token/lookup-accessor"
+      }
+    
+    read_accessors = requests.request(
+        "LIST", 
+        API_ENDPOINT['list_all_accessors'], 
+        headers=API_HEADER
+      ).json()
+    
+
+    """
+    store list of all accessors
+    """
+    get_accessors_keys = read_accessors['data']['keys']
+    
+    today = datetime.date.today()
+    
+    tokens_ok = []
+    tokens_critical = []
+    
+    tokens_dict = {}
+    
+    payload = {}
+    
+    for accessor in get_accessors_keys:
+      
+      payload["accessor"] = accessor
+
+      get_accessor_details = requests.request(
+          "POST", 
+          API_ENDPOINT['accessor_data'], 
+          data=json.dumps(payload), 
+          headers=API_HEADER
+        ).json()
+
+      # ignore tokens that never expires, and those which is issued to ldap users autmatically
+      if get_accessor_details['data']['expire_time'] is not None and get_accessor_details['data']['display_name'][:4] != 'ldap' :
+        
+        str_to_date = dateutil.parser.parse(get_accessor_details['data']['expire_time'])
+        days_left = str_to_date.date() - today
+        tokens_dict['days_left'] = days_left.days
+        tokens_dict['name'] = get_accessor_details['data']['display_name']
+
+        if '-' not in tokens_dict['name']:
+          tokens_dict['name'] = tokens_dict['name'] + "-" + get_accessor_details['data']['accessor'][:7]
+    
+        if days_left.days >= THRESHOLD_CRITICAL:
+          tokens_ok.append(tokens_dict.copy())
+        else:
+          tokens_critical.append(tokens_dict.copy())
+    
+    if tokens_critical:
+      for token in tokens_critical:
+        print (token['name'] + " expiring in " + str(token['days_left']) + " day(s)," )
+      sys.exit(2)
+    else:
+      for token in tokens_ok:
+        print (token['name'] + " expiring in " + str(token['days_left']) + " day(s)," )
+      sys.exit(0)
+
+if __name__ == "__main__":
+  f = VaultTokenExpire()
